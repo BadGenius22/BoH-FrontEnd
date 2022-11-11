24d03a0b00d791c925123ffa2ca73a8ea31290d6
BadGenius22 <badgenius.crypto@gmail.com>
2022-11-11 13:59:02 +0700

    2nd commit

--- src/contract/index.js
+++ src/contract/index.js
@@ -1,4 +1,4 @@
 import contract from "./BattleCard.json";
 
-export const ADDRESS = "0x8aEcdB7365177D24995c1878d7773e21152398E7";
+export const ADDRESS = "0xbe07528c063712b9390b0d35719f97ebf774386d";
 export const { abi: ABI } = contract;
